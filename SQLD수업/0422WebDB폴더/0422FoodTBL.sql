7994ef9e347068e3862596a8c8dc26c2226fc7e0
rlarjsgml1 <qhdudrjsgml@gmail.com>
2026-04-22 12:37:16 +0900

    0422 Web 수업 DB 자료

---
+++ SQLD수업/0422WebDB폴더/0422FoodTBL.sql
@@ -0,0 +1,24 @@
+create  table  foodtbl(
+    name varchar2(30) , 
+    price  varchar2(10)  
+);
+
+
+
+
+
+insert into foodtbl values( '스파게티' , 12000) ;
+insert into foodtbl values( '모듬초밥' , 20000) ;
+insert into foodtbl values( '수제햄버거세트' ,14000 ) ;
+insert into foodtbl values( '볶음밥' , 10000) ;
+insert into foodtbl values( '돈까스카레' , 12000) ;
+insert into foodtbl values( '갈비탕' ,16000 ) ;
+insert into foodtbl values( '피자' , 12000) ;
+insert into foodtbl values( '라면' , 5000) ;
+insert into foodtbl values( '참지김밥' ,6000 ) ;
+insert into foodtbl values( '소고기김밥' , 6500) ;
+insert into foodtbl values( '라볶이' , 6000) ;
+insert into foodtbl values( '튀김세트' ,6500 ) ;
+ 
+commit;
+
